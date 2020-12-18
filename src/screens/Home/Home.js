61904de96e4655147ead44cde4431d80d1068a91
Yogesh Kumar <kumaryogesh8490@gmail.com>
2020-12-18 19:52:28 +0530

    home page completed with filters

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,17 +1,172 @@
 import React, { Component } from 'react';
 import './Home.css';
-import Header from '../../common/Header/Header'
-import UpcomingMovies from '../UpcomingMovies/UpcomingMovies';
+import Header from '../../common/Header/Header';
+import genres from '../../common/genres';
+import artists from '../../common/artists';
+//import UpcomingMovies from '../UpcomingMovies/UpcomingMovies';
+import moviesData from '../../common/moviesData';
+import { withStyles } from '@material-ui/core/styles';
+import { GridList, GridListTile, GridListTileBar, IconButton, Card, CardContent, Typography, FormControl, InputLabel, Input, Select, MenuItem, ListItemText, Checkbox, TextField, Button } from '@material-ui/core'
+import InfoIcon from '@material-ui/icons/Info';
+import { PhotoSizeSelectLargeRounded, VerticalAlignCenter } from '@material-ui/icons';
 
-export class Home extends Component {
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: '#ff9999',
+        padding: '8px',
+        fontSize: '1rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240,
+    },
+    title: {
+        color: theme.palette.primary.light,
+    }
+});
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movieName: "",
+            genres: [],
+            artists: []
+        }
+    }
+
+    movieNameChangeHandler = event => {
+        this.setState({ movieName: event.target.value });
+    }
+
+    genreSelectHandler = event => {
+        this.setState({ genres: event.target.value });
+    }
+    artistSelectHandler = (e) => {
+        this.setState({ artists: e.target.value });
+    }
+
     render() {
+        const { classes } = this.props;
         return (
             <div>
                 <Header />
-                <UpcomingMovies />
+                <div className={classes.upcomingMoviesHeading}>
+                    <span>Upcoming Movies</span>
+                </div>
+                <GridList cols={5} className={classes.gridListUpcomingMovies} >
+                    {moviesData.map(movie => (
+                        <GridListTile key={movie.id}>
+                            <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                            <GridListTileBar title={movie.title} />
+                        </GridListTile>
+                    ))}
+                </GridList>
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                        <Card>
+                            <CardContent style={{ backgroundColor: '#D7DFDB' }}>
+                                <FormControl className={classes.formControl}>
+                                    <Typography className={classes.title} color="textSecondary">
+                                        FIND MOVIES BY
+                                    </Typography>
+                                </FormControl>
+
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                    <Input id="movieName" onChange={this.movieNameChangeHandler} />
+                                </FormControl>
+
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="select-multiple-checkbox-genres">Genres</InputLabel>
+                                    <Select
+                                        multiple
+                                        input={<Input id="select-multiple-checkbox-genres" />}
+                                        renderValue={selected => selected.join(',')}
+                                        value={this.state.genres}
+                                        onChange={this.genreSelectHandler}
+                                    >
+                                        <MenuItem value="0">None</MenuItem>
+                                        {genres.map(genre => (
+                                            <MenuItem key={genre.id} value={genre.name}>
+                                                <Checkbox checked={this.state.genres.indexOf(genre.name) > -1} />
+                                                <ListItemText primary={genre.name} />
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="select-multiple-checkbox-artists">Artists</InputLabel>
+                                    <Select
+                                        multiple
+                                        input={<Input id="select-multiple-checkbox-artists" />}
+                                        renderValue={selected => selected.join(',')}
+                                        value={this.state.artists}
+                                        onChange={this.artistSelectHandler}
+                                    >
+                                        <MenuItem value="0">None</MenuItem>
+                                        {artists.map(artist => (
+                                            <MenuItem key={artist.id} value={artist.first_name + " " + artist.last_name}>
+                                                <Checkbox checked={this.state.artists.indexOf(artist.first_name + " " + artist.last_name) > -1} />
+                                                <ListItemText primary={artist.first_name + " " + artist.last_name} />
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <TextField
+                                        id="releaseDateStart"
+                                        label="Release Date Start"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{
+                                            shrink: true
+                                        }} />
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <TextField
+                                        id="releaseDateEnd"
+                                        label="Release Date End"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{ shrink: true }} />
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <Button variant="contained" color="primary">Apply</Button>
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
 }
-
-export default Home;
+export default withStyles(styles)(Home);
